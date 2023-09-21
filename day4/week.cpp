0c2810c5ba9f4e36e25842ddf2805ef9e684635a
vikas148 <vb334531@gmail.com>
2023-09-21 12:13:10 +0530

    day4

---
+++ day4/week.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter your number"<<endl;
+    cin>>n;
+    if (n==1)
+    {
+        cout<<"monnday";
+    }
+    else if (n==2){
+        cout<<"Tuesday";
+    }
+
+ 
+    else if (n==3){
+        cout<<"wednesday";
+    }
+
+    else if (n==4){
+        cout<<"Thursday";
+    }
+    
+    else if (n==5){
+        cout<<"Friday";
+    }
+    else if (n==6){
+        cout<<"Saturday";
+    }
+    else if (n==7){
+        cout<<"sunday";
+    }
+    
+    else 
+    cout<<"Enter the right number";
+}
