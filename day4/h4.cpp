54ef7e503f203ecf5a99f5da903af180d02992be
vikas148 <vb334531@gmail.com>
2023-09-21 12:12:20 +0530

    day4

---
+++ day4/h4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your Age "<<endl;
+    cin>>age;
+    if (age<=12)
+    {
+        cout<<"YES";
+    }
+    else if (age>=65)
+    {
+        cout<<"YES";
+    }
+    else
+    cout<<"NO";
+    
+}
