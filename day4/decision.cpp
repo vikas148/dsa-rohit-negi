4618a14c7baac21be92232d2ece1ed7b25cffb44
vikas148 <vb334531@gmail.com>
2023-09-21 12:11:03 +0530

    day4

---
+++ day4/decision.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout<<"Enter your Number"<<endl;
+    cin>>number;
+
+    if (number%2==0)
+    {
+       cout<<"even";
+    }
+    else
+    cout<<"odd";
+    
+}
