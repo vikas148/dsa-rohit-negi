e4e8551343e3b9c9c18cec9d84267fab72b63bbf
vikas148 <vb334531@gmail.com>
2023-09-21 12:11:18 +0530

    day4

---
+++ day4/h1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two distnict numbers"<<endl;
+    cin>>a;
+    cin>>b;
+
+    if (a>b)
+    {
+        cout<<a <<"is greator than " <<b;
+    }
+    else
+    cout<<"b is greator than a";
+    
+}
