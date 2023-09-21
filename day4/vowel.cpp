7f898c0110df432c905a5f08afb325ee50bec8d1
vikas148 <vb334531@gmail.com>
2023-09-21 12:12:59 +0530

    day4

---
+++ day4/vowel.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"Enter your character"<<endl;
+    cin>>ch;
+    
+    if (ch=='a')
+    {
+        cout<<"vowel";
+    }
+
+    else if (ch=='e')
+    {
+        cout<<"vowel";
+    }
+    else if (ch=='i')
+    {
+        cout<<"vowel";
+    }
+    else if (ch=='o')
+    {
+        cout<<"vowel";
+    }
+    else if (ch=='u')
+    {
+        cout<<"vowel";
+    }
+    else
+    cout<<"consonant";
+}
