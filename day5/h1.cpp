04643a00585ac50bce3a06c5c303a0a5d822e6a8
vikas148 <vb334531@gmail.com>
2023-09-22 14:45:38 +0530

    day 5hw 1

---
+++ day5/h1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a;
+    cin>>b;
+    for(int i=a;i>=b;i--)
+    {
+
+     cout<<i<<endl; 
+        
+    }
+    
+}
