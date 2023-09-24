a7244d43d69578c806954ddfb08f1d8df343f4bd
vikas148 <vb334531@gmail.com>
2023-09-24 16:34:10 +0530

    pattern1

---
+++ day7/pattern1.cpp
@@ -0,0 +1,22 @@
+//print following pattern
+/*1234
+1234
+1234
+1234*/
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n;
+     cin>>n;
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<j;
+
+        }
+        cout<<endl;
+     }
+}
