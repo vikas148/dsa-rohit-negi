0a6e6ad5abb5d928afbfaa4db21d90e3eb904834
vikas148 <vb334531@gmail.com>
2023-09-24 16:34:46 +0530

    pattern5

---
+++ day7/pattern5.cpp
@@ -0,0 +1,24 @@
+//print following pattern
+/*
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+6 6 6 6 6 6 
+7 7 7 7 7 7 7 
+8 8 8 8 8 8 8 8
+*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
