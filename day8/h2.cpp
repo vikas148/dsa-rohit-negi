cc89ddeb92cf1b7caf541bd7b6ad358500038e38
vikas148 <vb334531@gmail.com>
2023-09-25 12:15:32 +0530

    homework

---
+++ day8/h2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using  namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        for(int column=1;column<=n;column++){
+            cout<<column*column*column<<" ";
+        }
+        cout<<endl;
+    }
+}
