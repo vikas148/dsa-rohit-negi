db3f94ee9f0578a1017967ccc543ac8241958696
vikas148 <vb334531@gmail.com>
2023-09-28 19:34:35 +0530

    day11

---
+++ day11/pattern4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int j;
+int main(){
+   int n;
+   cin>>n;
+   for(int i=n;i>=1;i--){
+    for( j=1;j<=i;j++){
+        cout<<"* ";
+    }
+    
+    for(j=1;j<=2*n-2*i;j++){
+        cout<<"  ";
+    }
+
+    for( j=1;j<=i;j++){
+        cout<<"* ";
+    }
+       cout<<endl;
+   } 
+
+
+    for(int i=1;i<=n;i++){
+    for( j=1;j<=i;j++){
+        cout<<"* ";
+    }
+    
+    for(j=1;j<=2*n-2*i;j++){
+        cout<<"  ";
+    }
+
+    for( j=1;j<=i;j++){
+        cout<<"* ";
+    }
+       cout<<endl;
+   }
+}   
