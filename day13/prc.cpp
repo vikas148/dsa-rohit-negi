d0faa0420e9f9e184897a9b7d6bd5e1cda039747
vikas148 <vb334531@gmail.com>
2023-09-30 20:17:57 +0530

    day13

---
+++ day13/prc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int temprature;
+    cout<<"What is temprature outside your home"<<endl;
+    cin>>temprature;
+    if(temprature<=90 && temprature>=70 ){
+        cout<<"YES"<<endl;
+    }
+
+    else{
+        cout<<"NO"<<endl;;
+    }
+
+}
