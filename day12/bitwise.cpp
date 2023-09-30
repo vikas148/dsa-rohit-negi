7ea21e403c42c29b4c8a1b60228880d54cf4f8ec
vikas148 <vb334531@gmail.com>
2023-09-30 20:18:04 +0530

    day12

---
+++ day12/bitwise.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //   int ans= 2&3;
+    //   int ans= 2|3;
+    //   int ans= 2^3;
+    //   int ans= 2<<3;
+    //   int ans= 2>>3;
+    //   int ans= ~5;
+      int ans= ~5;
+      cout<<ans;
+}
