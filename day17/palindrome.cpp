c62bff579b082250ac127a9872a585bf8e276ddc
vikas148 <vb334531@gmail.com>
2023-10-04 16:18:39 +0530

    day17

---
+++ day17/palindrome.cpp
@@ -0,0 +1,16 @@
+//logic
+
+//   int num =x,rem,ans=0;
+//         while(num)
+//         {
+//             rem=num%10;
+//             num/=10;
+//             if(ans>INT_MAX/10)
+//             return false;
+//             ans=ans*10+rem;
+//         }
+//         if(ans==x)
+//         return true;
+//         else
+//         return false;
+//     }
