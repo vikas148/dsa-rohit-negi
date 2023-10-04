5523d445471cb8e33b40ac3d556cc3f639a8c547
vikas148 <vb334531@gmail.com>
2023-10-04 16:18:17 +0530

    day17

---
+++ day17/complement.cpp
@@ -0,0 +1,17 @@
+//logic
+
+//  int ans=0,rem,mul=1;
+// if(n==0)
+        // return 1;
+//         while(n)
+//         {
+//             rem=n%2;
+//             rem=rem^1;
+//             n/=2;
+//             ans= ans+rem*mul;
+//             mu=mul*2;
+
+//         }
+        
+//         return ans;
+//     }
