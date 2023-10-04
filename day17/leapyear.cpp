f0f0337641940c2b5231e2e5e14dab5081dfebd8
vikas148 <vb334531@gmail.com>
2023-10-04 16:18:26 +0530

    day17

---
+++ day17/leapyear.cpp
@@ -0,0 +1,13 @@
+//logic
+
+//  if(N%400==0){
+//         return 1;
+//     }
+    
+//     else if(N%4==0 && N%100!=0){
+//         return 1;
+//     }
+
+//      else{
+//          return 0;
+//      }
