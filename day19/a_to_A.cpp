e295f2255c107f719f202002689a781044655f4f
vikas148 <vb334531@gmail.com>
2023-10-06 22:07:01 +0530

    day19

---
+++ day19/a_to_A.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+char convert(char name){
+    char ans=name-'a'+'A';
+    return ans;
+}
+
+int main(){
+char name;
+cin>>name;
+cout<<convert(name)<<endl;
+}
