19c3d9510f42efeccd9997bc594d550a6c65812e
vikas148 <vb334531@gmail.com>
2023-10-09 10:21:44 +0530

    day22

---
+++ day22/arr_min.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main() {
+    
+    int arr[5]={8,4,3,5,1};
+    int ans=INT_MAX;
+    for(int i=0;i<5;i++){
+        if(arr[i]<ans){
+            ans=arr[i];
+        }
+      
+    }
+     cout<<ans<<endl;
+    return 0;
+}
