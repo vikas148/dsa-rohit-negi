f4ee0cf088e54deec7384dcf60c688712c927f62
vikas148 <vb334531@gmail.com>
2023-10-16 16:20:34 +0530

    day 29

---
+++ day29/insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the size of an array" << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+            }
+            else
+                break;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
