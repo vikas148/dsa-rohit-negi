1e80cea5bdd13619053f78cb76303bbc5827848e
vikas148 <vb334531@gmail.com>
2023-10-25 00:18:16 +0530

    day37

---
+++ day37/vector.cpp
@@ -0,0 +1,52 @@
+
+// vector<daatatype>name(size_of_vector,initilize)
+//  vector<int>vikas(4)
+
+// vector<int>vikas(4,2), 2,2,2,2,
+
+//how to insert vaalue in vector
+// vector<int>a;
+// a.push_back(4)
+// a.push_back(8)
+
+//vector<int>v={2,4,6,8,10}
+
+//vector<int>v(5);
+//for(i=0;i<5;i++)
+//   cin>>v[i]
+
+//how to remove value
+// v.pop_back()   to remove last value
+//v.clear()   clear all array
+//v.erase(v.begin()+2)  to remove inndex 2 element 
+
+//size and capacity kya hai 
+
+//front , back , at ,empty
+// cout<<v.front();  array ke front ki value dega
+// cout<<v.back();  array ke end ki value dega
+// cout<<v.empty(); bool type checks whether vector  is empty or not
+//cout<<v.at
+
+//iterator in a vector
+// v.begin() ; points to begining index of vector
+//v.end(); points to last index+1 position of vector
+
+//    for(auto it=v.begin(); it !=v.end();i++) 
+    //   cout<<*it<<" ";
+
+//v.rbegin()  reverse begin
+//v.rend() reverse end
+
+
+//for(i=0;i<v.size();i++)
+//cout<<v[i];
+
+
+// sorting in vector
+
+//sort(v.begin(),v.end()); sort in increasing  order
+//sort(v.begin(),v.end(), greater<int>); sort in decreasing order
+
+
+
