b0c6bc53f6361b55edbca7fdb82dc1ad9f354de0
vikas148 <vb334531@gmail.com>
2023-10-27 18:07:33 +0530

    day40

---
+++ day40/trappingrain.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+       int n= height.size();
+       int leftmax=0, rightmax=0,maxheight=height[0],index=0,water=0;
+
+      for(int  i=1;i<n;i++){
+          if(maxheight<height[i]){
+              maxheight=height[i];
+              index=i;
+          }
+      }
+//left part
+
+for(int i=0;i<index;i++){
+     if(leftmax>height[i])
+     water +=leftmax-height[i];
+     else
+     leftmax=height[i];
+}
+
+//right
+
+for(int i=n-1;i>index;i--){
+     if(rightmax>height[i])
+     water +=rightmax-height[i];
+     else
+     rightmax=height[i];
+}
+
+return water;
+
+    }
+};
