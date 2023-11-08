ba10e30ae36c8ec4a33cfcd10e9aaeae736ef62b
vikas148 <vb334531@gmail.com>
2023-11-08 17:37:10 +0530

    day46

---
+++ day46/binarysearch2drowsort.cpp
@@ -0,0 +1,65 @@
+// // Given a matrix of size n x m, where every row and column is sorted in increasing order, and a number x. Find whether element x is present in the matrix or not.
+
+// //{ Driver Code Starts
+// #include<bits/stdc++.h> 
+// using namespace std;
+
+// // } Driver Code Ends
+
+
+// class Solution
+// {
+//     public:
+//     //Function to search a given number in row-column sorted matrix.
+//     bool search(vector<vector<int> > matrix, int n, int m, int x) 
+//     {
+//         // code here 
+//         int row=0, col =m-1;
+//         while(row<n && col>=0)
+//         {
+//             if(matrix[row][col]==x)
+//             return 1;
+//             else if(matrix[row][col]<x)
+//             row++;
+//             else
+//             col--;
+            
+//         }
+//         return 0;
+//     }
+// };
+
+// //{ Driver Code Starts.
+// int main() 
+// { 
+// 	int t;
+//     cin>>t;
+//     while(t--)
+//     {
+
+//  		int n, m;
+//     	cin>> n>> m;
+//         vector<vector<int> > matrix(n);
+            
+//         for(int i=0; i<n; i++)
+//         {
+//             matrix[i].assign(m, 0);
+//             for( int j=0; j<m; j++)
+//             {
+//                 cin>>matrix[i][j];
+//             }
+//         }
+        
+//         int x;
+//         cin>>x;
+//         Solution obj;
+//         if (obj.search(matrix,n,m,x)) 
+//             cout << "1\n"; 
+//         else
+//             cout << "0\n"; 
+//     }
+// 	return 0; 
+// } 
+
+
+// // } Driver Code Ends
