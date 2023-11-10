7423670f409c4499d1bfb512f5fca5f4a4c18a58
vikas148 <vb334531@gmail.com>
2023-11-10 18:13:33 +0530

    day50

---
+++ day50/string2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int arr[20];
+    // cin>>arr;
+    // arr[2]='\0';
+    // cout<<arr;
+
+
+    // string s="rohit negi is a /"good/" boy";
+    // cout<<s;
+
+    string  s;
+    cin>>s;
+    cout<<s;
+}
