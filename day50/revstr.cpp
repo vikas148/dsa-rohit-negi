4cb66a36ffbb31abea96934c5e4c8b25c898679d
vikas148 <vb334531@gmail.com>
2023-11-10 18:13:14 +0530

    day50

---
+++ day50/revstr.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ string s="Rohit";
+ int start =0, end = s.size()-1;
+while(start<end)
+{
+    swap(s[start],s[end]);
+    start++,end--;
+}
+cout<<s;
+
+}
