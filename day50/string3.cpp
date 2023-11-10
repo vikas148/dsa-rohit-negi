8b9f7a5882d5d5526f5c7c5cb6e15785b7f8042b
vikas148 <vb334531@gmail.com>
2023-11-10 18:13:40 +0530

    day50

---
+++ day50/string3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  //to store lines and paragraphs in string
+  string s;
+//   getline(cin,s);
+//   cout<<s;
+//to get the size of string
+
+// cout<<s.size();
+
+//concatination of strings
+ string s1;
+ cin>>s1;
+
+ cout<<s+s1;
+
+ //pushback and pop back operations on strings
+
+string s2="rohit";
+
+s2.push_back('p');    //rohitp
+s2.pop_back();        //rohi 
+
+
+
+}
