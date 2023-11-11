7374ae82fc0b39f2acb21cfec44be69c8f922f65
vikas148 <vb334531@gmail.com>
2023-11-11 22:03:29 +0530

    day51

---
+++ day51/sortstr.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string sort(string s);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        cout << sort(s) << endl;
+    }
+return 0;
+}
+
+
+
+
+// } Driver Code Ends
+
+
+string sort(string s){
+    //complete the function here
+    vector<int>alpha(26,0);
+    for(int i=0; i<s.size();i++)
+    {
+        alpha[s[i]-'a']++;
+    }
+    
+    string ans;
+    for(int i=0; i<26;i++)
+    {
+       char c = 'a'+i;
+       while(alpha[i])
+       {
+           ans+=c;
+           alpha[i]--;
+       }
+    }
+    return ans;
+}
