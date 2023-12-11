b969343445ae7fff4b1677bc4cf07f4d418f2aae
vikas148 <vb334531@gmail.com>
2023-12-11 14:49:58 +0530

    day78

---
+++ day78/minelemarr.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int minElement(int arr[],int index,int n)
+{
+    if(index==n-1)
+    return arr[index];
+   return  min(arr[index], minElement(arr,index+1,n));
+}
+int main()
+{
+    int arr[]={7,2,4,1,6};
+   cout<<minElement(arr,0,5);
+}
