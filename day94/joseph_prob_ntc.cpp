6f782c211a4b654f7d8de848370a80116d5ab3ec
vikas148 <vb334531@gmail.com>
2023-12-27 20:32:41 +0530

    commented code

--- day94/joseph_prob_ntc.cpp
+++ day94/joseph_prob_ntc.cpp
@@ -1,15 +1,15 @@
 
-// with order of n complexity
-class Solution {
-public:
+// // with order of n complexity
+// class Solution {
+// public:
    
- int winner(int n, int k)
- {
-     if(n==1)
-     return 0;
-    return (winner(n-1,k)+k)%n;
- }
-    int findTheWinner(int n, int k) {
-      return winner(n,k)+1;
-    }
-};
+//  int winner(int n, int k)
+//  {
+//      if(n==1)
+//      return 0;
+//     return (winner(n-1,k)+k)%n;
+//  }
+//     int findTheWinner(int n, int k) {
+//       return winner(n,k)+1;
+//     }
+// };
