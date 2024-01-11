1e08d5f5855a31302628f8f12cf33be303f78c4b
vikas148 <vb334531@gmail.com>
2024-01-11 20:32:12 +0530

    day108

---
+++ day108/exceptionhand.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class customer{
+string name;
+int balance , account_number;
+public:
+customer(string name, int balance, int account_number)
+{
+    this->name=name;
+    this->balance=balance;
+    this->account_number=account_number;
+}
+
+void deposit(int amount){
+    if(amount>0)
+    {
+        balance+=amount;
+        cout<<amount <<" is credited successfully"<<endl;
+    }
+    else{
+        cout<<"amount should be greater than 0\n";
+    }
+}
+
+void withdraw(int amount)
+{
+    if(amount>0&&amount<=balance)
+    {
+     balance-=amount;
+     cout<<amount<<"is debited sucessfully"<<endl;
+
+    }
+    else if(amount<0)
+    {
+        throw"amount should be greater than 0\n";
+    }
+
+    else{
+        throw "your balance is low\n";
+    }
+}
+};
+int main()
+{
+
+    customer c1("rohit",5000,10);
+    try{c1.deposit(100);
+    c1.withdraw(6000);
+    c1.deposit(100);
+    }
+    catch(const char*e)
+    {
+        cout<<"Exception occured: "<<e<<endl;
+    } 
+}
