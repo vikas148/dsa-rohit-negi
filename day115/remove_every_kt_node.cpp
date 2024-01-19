34cc3e1e243d1a3355d5ce6155c1a01bcd3fe02d
vikas148 <vb334531@gmail.com>
2024-01-19 20:39:33 +0530

    minor changes

--- day115/remove_every_kt_node.cpp
+++ day115/remove_every_kt_node.cpp
@@ -1,113 +1,113 @@
-//{ Driver Code Starts
-// C program to find n'th Node in linked list
-#include <stdio.h>
-#include <stdlib.h>
-#include<iostream>
-using namespace std;
-/* Link list Node */
-struct Node
-{
-    int data;
-    struct Node* next;
+// //{ Driver Code Starts
+// // C program to find n'th Node in linked list
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include<iostream>
+// using namespace std;
+// /* Link list Node */
+// struct Node
+// {
+//     int data;
+//     struct Node* next;
     
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
+//     Node(int x){
+//         data = x;
+//         next = NULL;
+//     }
     
-};
+// };
 
-Node *deleteK(Node *,int);
+// Node *deleteK(Node *,int);
 
-void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
-{
-    struct Node* new_node = new Node(new_data);
+// void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+// {
+//     struct Node* new_node = new Node(new_data);
     
-    if (*head_ref == NULL)
-       *head_ref = new_node;
-    else
-       (*tail_ref)->next = new_node;
+//     if (*head_ref == NULL)
+//        *head_ref = new_node;
+//     else
+//        (*tail_ref)->next = new_node;
     
-    *tail_ref = new_node;
-}
+//     *tail_ref = new_node;
+// }
 
-/* Function to get the middle of the linked list*/
-int getMiddle(struct Node *head);
+// /* Function to get the middle of the linked list*/
+// int getMiddle(struct Node *head);
 
-/* Driver program to test above function*/
-int main()
-{
-  int T,i,n,l;
-    cin>>T;
-    while(T--){
-    struct Node *head = NULL,  *tail = NULL;
-        cin>>n;
-        for(i=1;i<=n;i++)
-        {
-            cin>>l;
-            append(&head, &tail, l);
-        }
-        int K;
-        cin>>K;
-        Node *res = deleteK(head,K);
-        Node *temp = res;
-        while(temp!=NULL)
-        {
-            cout<<temp->data<<" ";
-            temp=temp->next;
-        }
-        cout<<endl;
-    }
-    return 0;
-}
+// /* Driver program to test above function*/
+// int main()
+// {
+//   int T,i,n,l;
+//     cin>>T;
+//     while(T--){
+//     struct Node *head = NULL,  *tail = NULL;
+//         cin>>n;
+//         for(i=1;i<=n;i++)
+//         {
+//             cin>>l;
+//             append(&head, &tail, l);
+//         }
+//         int K;
+//         cin>>K;
+//         Node *res = deleteK(head,K);
+//         Node *temp = res;
+//         while(temp!=NULL)
+//         {
+//             cout<<temp->data<<" ";
+//             temp=temp->next;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
 
-// } Driver Code Ends
+// // } Driver Code Ends
 
 
-/* Link list Node 
+// /* Link list Node 
 
-struct Node
-{
-    int data;
-    struct Node* next;
+// struct Node
+// {
+//     int data;
+//     struct Node* next;
     
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
+//     Node(int x){
+//         data = x;
+//         next = NULL;
+//     }
     
-};
-*/
+// };
+// */
 
-/*You are required to complete this method*/
+// /*You are required to complete this method*/
 
-/* Function to get the middle of the linked list*/
-/*K will always be in range */
+// /* Function to get the middle of the linked list*/
+// /*K will always be in range */
 
-Node* deleteK(Node *head,int k)
-{
-  //Your code here
+// Node* deleteK(Node *head,int k)
+// {
+//   //Your code here
   
-  if(k==1)
-  return NULL;
-  Node * curr=head, *prev=NULL;
-  int count =1;
+//   if(k==1)
+//   return NULL;
+//   Node * curr=head, *prev=NULL;
+//   int count =1;
   
-  while(curr)
-  {
-    if(k==count)
-    {
-        prev->next=curr->next;
-        delete curr;
-        curr=prev->next;
-        count=1;
-    }
-    else
-    {
-        prev=curr;
-        curr=curr->next;
-        count++;
-    }
-  }
-  return head;
-}
+//   while(curr)
+//   {
+//     if(k==count)
+//     {
+//         prev->next=curr->next;
+//         delete curr;
+//         curr=prev->next;
+//         count=1;
+//     }
+//     else
+//     {
+//         prev=curr;
+//         curr=curr->next;
+//         count++;
+//     }
+//   }
+//   return head;
+// }
--- day115/remove_nt_node.cpp
+++ day115/remove_nt_node.cpp
@@ -1,48 +1,48 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int count =0;
-        ListNode *temp= head;
-        while(temp)
-        {
-            count++;
-            temp=temp->next;
-        };
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int count =0;
+//         ListNode *temp= head;
+//         while(temp)
+//         {
+//             count++;
+//             temp=temp->next;
+//         };
         
       
-         count-=n;
-         if(count == 0)
-        {
-           temp=head;
-           head=head->next;
-           delete temp;
-           return head; 
-        }
-          ListNode *curr=head, *prev =NULL;
+//          count-=n;
+//          if(count == 0)
+//         {
+//            temp=head;
+//            head=head->next;
+//            delete temp;
+//            return head; 
+//         }
+//           ListNode *curr=head, *prev =NULL;
 
-          while(count--)
-          {
-              prev=curr;
-              curr=curr->next;
+//           while(count--)
+//           {
+//               prev=curr;
+//               curr=curr->next;
 
-          }
+//           }
 
         
-          prev->next=curr->next;
-          delete curr;
-          return head;
+//           prev->next=curr->next;
+//           delete curr;
+//           return head;
 
-    }
+//     }
     
 
-};
+// };
